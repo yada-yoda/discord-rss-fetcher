910c06415bcf1a94b1db345fa6c2749b5b3eafb6
benji7425 <benji7425@outlook.com>
2016-10-30 19:39:46 +0000

    Added error handling for invalid RSS feed

--- feed-bot.js
+++ feed-bot.js
@@ -26,27 +26,36 @@ bot.on("message", function (user, userID, channelID, message) {
 
 function checkFeedAndPost() {
 	//check the feed, with a callback
-	FeedRead(Config.feedUrl, function (err, articles) {
+	FeedRead(Config.feedUrl, function (err, articles){
+		try{
+			checkLinkAndPost(err, articles);
+		}
+		catch(ex){
+			console.log("ERROR: " + (ex.message || ex));
+		}
+	});
+}
+
+function checkLinkAndPost(err, articles) {
+	if (err) throw "Error reading RSS feed: " + (err.message || err);
+
+	var latestLink = articles[0].link;
+
+	//get the latest 100 messages (100 is the limit)
+	bot.getMessages({
+		channelID: Config.channelID,
+		limit: 100
+	}, function (err, messages) {
 		if (err) throw err;
 
-		var latestLink = articles[0].link;
+		//get an array of strings from the array of message objects
+		var messageContents = messages.map((message) => { return message.content; });
 
-		//get the latest 100 messages (100 is the limit)
-		bot.getMessages({
-			channelID: Config.channelID,
-			limit: 100
-		}, function (err, messages) {
-			if (err) throw err;
-
-			//get an array of strings from the array of message objects
-			var messageContents = messages.map((message) => { return message.content; });
-
-			//if the messageContents array doesn't include the latest link, post it
-			if (!messageContents.includes(latestLink))
-				bot.sendMessage({
-					to: Config.channelID,
-					message: latestLink
-				});
-		});
+		//if the messageContents array doesn't include the latest link, post it
+		if (!messageContents.includes(latestLink))
+			bot.sendMessage({
+				to: Config.channelID,
+				message: latestLink
+			});
 	});
 }
