6a4f2a32d81b6f91ee6c377a0c468a59b79d36f5
benji7425 <benji7425@outlook.com>
2017-07-27 07:50:44 +0100

    Add loading data from json file

--- app/index.js
+++ app/index.js
@@ -1,9 +1,24 @@
+//node imports
+const FileSystem = require("fs");
+
+//external lib imports
+const JSONFile = require("jsonfile");
+
+//app component imports
+const GuildData = require("./models/guild-data.js");
+
+const SAVE_FILE = "./guilds.json";
+
 //acts as on ready function
 module.exports = (client) => {
-	client.on("message", message => message.reply("Hello!"));
-	//check messages in channel for links posted since last online
-	
+	const guildsData = FileSystem.existsSync(SAVE_FILE) ? parseJSON(JSONFile.readFileSync(SAVE_FILE)) : {}; //pull saved data from file
+
 	//set up an interval to check all the feeds
 
 	//set up an on message handler to detect when links are posted
 };
+
+function parseJSON(json) {
+	const guildIDs = Object.keys(json);
+	guildIDs.forEach(guildID => { guildIDs[guildID] = new GuildData(guildIDs[guildID]); });
+}
--- app/models/guild-data.js
+++ app/models/guild-data.js
@@ -1,6 +1,8 @@
+const Feed = require("./feed.js");
+
 module.exports = class GuildData {
 	constructor({id, feeds}) {
 		this.id = id;
-		this.feeds = feeds
+		this.feeds = feeds.filter(feed => new Feed(feed));
 	}
 };
