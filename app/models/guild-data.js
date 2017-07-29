36aa5639abe9d8c887b2f272c4ed58d71e1aae6f
benji7425 <benji7425@outlook.com>
2017-07-29 20:32:33 +0100

    Add method for recursively iterating over feeds in guild

--- app/models/guild-data.js
+++ app/models/guild-data.js
@@ -1,8 +1,21 @@
-const Feed = require("./feed.js");
+const FeedData = require("./feed-data.js");
+const Util = require("discordjs-util");
 
 module.exports = class GuildData {
-	constructor({id, feeds}) {
+	constructor({ id, feeds }) {
 		this.id = id;
-		this.feeds = feeds.filter(feed => new Feed(feed));
+		this.feeds = feeds.filter(feed => new FeedData(feed));
+	}
+
+	cachePastPostedLinks() {
+		let i = 0;
+		const recurse = () => {
+			this.feeds[i++].cachePastPostedLinks(this)
+				.catch(Util.dateError)
+				.then(recurse);
+			if (i > this.feeds.length)
+				return Promise.resolve();
+		};
+
 	}
 };
