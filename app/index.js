6b9bf2f27f091f968b35c209802a32b8f70fa60c
benji7425 <benji7425@outlook.com>
2017-08-12 01:49:35 +0100

    Update new index.js and config.json with application code

--- app/index.js
+++ app/index.js
@@ -7,6 +7,10 @@ const JsonFile = require("jsonfile");
 //my imports
 const DiscordUtil = require("discordjs-util");
 
+//app component imports
+const GuildData = require("./models/guild-data.js");
+const FeedData = require("./models/feed-data.js");
+
 //global vars
 const SAVE_FILE = "./guilds.json";
 
@@ -15,6 +19,8 @@ module.exports = (client) => {
 
 	const guildsData = FileSystem.existsSync(SAVE_FILE) ? fromJSON(JsonFile.readFileSync(SAVE_FILE)) : {};
 	setInterval(() => writeFile(guildsData), config.saveIntervalSec * 1000);
+
+	parseLinksInAllGuilds(client.guilds, guildsData).then(writeFile(guildsData));
 
 	client.on("message", message => {
 		if (message.author.id !== client.user.id) { //check the bot isn't triggering itself
@@ -41,15 +47,75 @@ const HandleMessage = {
 				case config.commands.version:
 					message.reply("v" + require("../package.json").version);
 					break;
+				case config.commands.addFeed:
+					addFeed(client, guildsData, message);
+					break;
 			}
 		}
 	}
 };
+
+function addFeed(client, guildsData, message) {
+	const feedData = createNewFeed(message); //create a new feed data instance from the data in our message
+
+	//ask the user if they're happy with the details they set up, save if yes, don't if no
+	DiscordUtil.ask(client, message.channel, message.member, "Are you happy with this? " + feedData)
+		.then(responseMessage => {
+
+			//if they responded yes, save the feed and let them know, else tell them to start again
+			if (message.content.toLowerCase() === "yes") {
+				saveFeed(guildsData, message.guild.id, feedData);
+				responseMessage.reply("Your new feed has been saved!");
+			}
+			else
+				responseMessage.reply("Your feed has not been saved, please add it again with the correct details");
+
+		});
+}
+
+function parseLinksInAllGuilds(guilds, guildsData) {
+	const promises = [];
+	for (let guild of guilds) {
+		const guildData = guildsData[guild.id];
+		if (guildData)
+			promises.push(guildData.cachePastPostedLinks());
+	}
+	return Promise.all(promises);
+}
+
+/**
+ * Create a new feed from the message object where the user is setting it up
+ * @param {Discord.Message} message 
+ * @returns {FeedData} Newly created feed data object
+ */
+function createNewFeed(message) {
+	const parameters = message.content.split(" "); //expect !addfeed <url> <channelName> <roleName>
+	const feedData = new FeedData({
+		link: parameters[1],
+		channelName: parameters[2],
+		roleName: parameters[3]
+	});
+	return feedData;
+}
+
+/**
+ * Saves a passed feed data object into the passed guildsData object, for the specified guild
+ * @param {object} guildsData 
+ * @param {string} guildID 
+ * @param {FeedData} feedData 
+ */
+function saveFeed(guildsData, guildID, feedData) {
+	if (!guildsData[guildID])
+		guildsData[guildID] = new GuildData({ id: guildID, feeds: [] });
+
+	guildsData[guildID].feeds.push(feedData);
+}
 
 function writeFile(guildsData) {
 	JsonFile.writeFile(SAVE_FILE, guildsData, err => { if (err) DiscordUtil.dateError("Error writing file", err); });
 }
 
 function fromJSON(json) {
-	throw "Not implemented";
+	const guildIDs = Object.keys(json);
+	guildIDs.forEach(guildID => { guildIDs[guildID] = new GuildData(guildIDs[guildID]); });
 }
