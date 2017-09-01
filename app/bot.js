939e6f5946309b76304c9601ba88e3bb95c31b79
benji7425 <benji7425@outlook.com>
2017-09-01 18:19:48 +0100

    Add commands to config

--- app/bot.js
+++ app/bot.js
@@ -3,11 +3,26 @@ const GuildData = require("./models/guild-data.js");
 module.exports = {
 	onCommand(commandObj, commandsObj, params, guildData, message) {
 		switch (commandObj.command) {
-			case commandsObj.commandName.command:
-				return; //return promise!
+			case commandsObj.addFeed.command:
+				return addFeed();
+			case commandsObj.removeFeed.command:
+				return removeFeed();
+			case commandsObj.viewFeeds.command:
+				return viewFeeds();
 		}
 	},
 	onNonCommandMsg(message, guildData) {
 		return;
 	}
 };
+
+function addFeed() {
+	//todo
+}
+
+function removeFeed() {
+	//todo
+}
+function viewFeeds() {
+	//todo
+}
