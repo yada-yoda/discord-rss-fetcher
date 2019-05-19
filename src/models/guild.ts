fe3e57ce9fd11f7679d4a10427e9d3f3dd0028aa
Benji Higgins <benji7425@pm.me>
2019-05-19 01:57:53 +0100

    Update Guild model to correctly set default feeds array value

--- src/models/guild.ts
+++ src/models/guild.ts
@@ -3,13 +3,15 @@ import Feed from "./feed";
 
 export default class Guild extends BotGuild
 {
-    public get feeds(): Feed[]
-    {
-        if (!this.record.feeds)
-            this.record.feeds = []
-        return Feed.getArrayProxy(this.record.feeds, this, "feeds", Feed)
-    }
+    public get feeds(): Feed[] { return Feed.getArrayProxy(this.record.feeds, this, "feeds", Feed) }
     public set feeds(value: Feed[]) { this.record.feeds = value }
 
     public get channels() { return this.djs.channels }
+
+    public loadRecord(record: any)
+    {
+        super.loadRecord(record)
+        if (!this.record.feeds)
+            this.record.feeds = []
+    }
 }
